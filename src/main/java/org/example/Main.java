7112509c01772abb1aa34cb92894f412a91227fc
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-17 09:08:41 +0100

    Feat Controller and MovieController

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.confing.DBManager;
+import org.example.controller.MovieController;
 import org.example.model.Movie;
 import org.example.repository.MovieRepositoryImpl;
 
@@ -15,10 +16,16 @@ public class Main {
        // DBManager.initConnection();
        // DBManager.closeConnection();
 
-        MovieRepositoryImpl movieRepository = new MovieRepositoryImpl();
 
-        Movie movie1 = new Movie("Película de prueba", 2026, 6.5,"url", "descripción de la película de prueba");
-        movieRepository.createMovie(movie1);
+
+        //Movie movie1 = new Movie("Película de prueba", 2026, 6.5,"url", "descripción de la película de prueba");
+        //movieRepository.createMovie(movie1);
+
+        Movie movie2 = new Movie("Película de prueba 2", 2025, 8.5,"url", "descripción de la película de prueba 2");
+
+        MovieRepositoryImpl movieRepository = new MovieRepositoryImpl();
+        MovieController movieController = new MovieController(movieRepository);
+        movieController.createMovieController(movie2);
     }
 
 }
