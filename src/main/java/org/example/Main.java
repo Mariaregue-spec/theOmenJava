c9adac20fa269d46d10e278f75adb4c42de853e6
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-17 10:19:08 +0100

    Feat Scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.example.confing.DBManager;
 import org.example.controller.MovieController;
 import org.example.model.Movie;
 import org.example.repository.MovieRepositoryImpl;
+import org.example.view.MovieView;
 
 import java.sql.SQLException;
 
@@ -21,11 +22,13 @@ public class Main {
         //Movie movie1 = new Movie("Película de prueba", 2026, 6.5,"url", "descripción de la película de prueba");
         //movieRepository.createMovie(movie1);
 
-        Movie movie2 = new Movie("Película de prueba 2", 2025, 8.5,"url", "descripción de la película de prueba 2");
+        //Movie movie2 = new Movie("Película de prueba 2", 2025, 8.5,"url", "descripción de la película de prueba 2");
 
-        MovieRepositoryImpl movieRepository = new MovieRepositoryImpl();
-        MovieController movieController = new MovieController(movieRepository);
-        movieController.createMovieController(movie2);
+        MovieRepositoryImpl movieRepository = new MovieRepositoryImpl();//esto lo gestionará Springboot
+        MovieController movieController = new MovieController(movieRepository);//esto lo gestionará Springboot
+        //movieController.createMovieController(movie2);
+        MovieView movieView = new MovieView(movieController);
+        movieView.CreateMovieView();
     }
 
 }
